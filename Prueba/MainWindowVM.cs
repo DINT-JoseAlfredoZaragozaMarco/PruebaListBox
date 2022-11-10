60f822b17500d18c6fd43f7a16340ab733904d6b
JOSÉ ALFREDO ZARAGOZA MARCO <joszarmar@alu.edu.gva.es>
2022-11-10 12:06:03 +0100

    Agregar archivos de proyecto.

---
+++ Prueba/MainWindowVM.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private ObservableCollection<String> provincias;
+
+        public ObservableCollection<String> Provincias
+        {
+            get { return provincias; }
+            set 
+            { 
+                provincias = value;
+                NotifyPropertyChanged("Provincias");
+            }
+        }
+
+        private String provinciaSelecccionada;
+
+        public String ProvinciaSeleccionada
+        {
+            get { return provinciaSelecccionada; }
+            set 
+            { 
+                provinciaSelecccionada = value;
+                NotifyPropertyChanged("ProvinciaSeleccionada");
+            }
+        }
+
+        private ObservableCollection<PersonaINotify> personas;
+
+        public ObservableCollection<PersonaINotify> Personas
+        {
+            get { return personas; }
+            set 
+            { 
+                personas = value;
+                NotifyPropertyChanged("Personas");
+            }
+        }
+
+        private PersonaINotify personaSeleccionada;
+
+        public PersonaINotify PersonaSeleccionada
+        {
+            get { return personaSeleccionada; }
+            set 
+            { 
+                personaSeleccionada = value;
+                NotifyPropertyChanged("PersonaSeleccionada");
+            }
+        }
+
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MainWindowVM()
+        {
+            Provincias = new ObservableCollection<string>();
+            Personas = new ObservableCollection<PersonaINotify>();
+
+            Provincias.Add("Alicante");
+            Provincias.Add("Valencia");
+            Provincias.Add("Castellón");
+
+            Personas.Add(new PersonaINotify("Jose", 20));
+            Personas.Add(new PersonaINotify("Javier", 24));
+            Personas.Add(new PersonaINotify("Ian", 21));
+        }
+
+        public void AnexionarMurcia()
+        {
+            if (!Provincias.Contains("Murcia"))
+            {
+                Provincias.Add("Murcia");
+            }
+        }
+    }
+}
---
+++ Prueba/Persona.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Prueba
+{
+    public class PersonaINotify : INotifyPropertyChanged
+    {
+        //Propiedades
+        //En caso de que se cambie el valor, se debe llamar al método que lanza el evento PropertyChanged
+        private string _nombre;
+        public string Nombre
+        {
+            get { return this._nombre; }
+            set
+            {
+                if (this._nombre != value)
+                {
+                    this._nombre = value;
+                    this.NotifyPropertyChanged("Nombre");
+                }
+            }
+        }
+
+        private int _edad;
+        public int Edad
+        {
+            get { return this._edad; }
+            set
+            {
+                if (this._edad != value)
+                {
+                    this._edad = value;
+                    this.NotifyPropertyChanged("Edad");
+                }
+            }
+        }
+
+        //Constructores
+        public PersonaINotify()
+        {
+            Nombre = "";
+            Edad = 0;
+        }
+        public PersonaINotify(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+
+        //Implementación de la interfaz INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public override string ToString()
+        {
+            return Nombre + " - " + Edad;
+        }
+    }
+}
